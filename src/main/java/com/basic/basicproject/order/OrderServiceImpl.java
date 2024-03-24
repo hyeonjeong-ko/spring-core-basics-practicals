a10ebc0e37d9e74a969c3b5404faaf850ca9df5b
Kohyeonjeong <toyu7870@naver.com>
2024-03-24 20:00:35 +0900

    feat: Order & Discount Domain

---
+++ src/main/java/com/basic/basicproject/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package com.basic.basicproject.order;
+
+import com.basic.basicproject.discount.DiscountPolicy;
+import com.basic.basicproject.discount.FixDiscountPolicy;
+import com.basic.basicproject.member.Member;
+import com.basic.basicproject.member.MemberRepository;
+import com.basic.basicproject.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 최종 할인 가격
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
