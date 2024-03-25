4aef6e1554904f652fba52edddcb558a2057e14e
Kohyeonjeong <toyu7870@naver.com>
2024-03-26 01:25:32 +0900

    feat: Rate Policy Change

---
+++ src/main/java/com/basic/basicproject/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.basic.basicproject.discount;
+
+import com.basic.basicproject.member.Grade;
+import com.basic.basicproject.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else{
+            return 0;
+        }
+    }
+}
--- src/main/java/com/basic/basicproject/order/OrderServiceImpl.java
+++ src/main/java/com/basic/basicproject/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.basic.basicproject.order;
 
 import com.basic.basicproject.discount.DiscountPolicy;
 import com.basic.basicproject.discount.FixDiscountPolicy;
+import com.basic.basicproject.discount.RateDiscountPolicy;
 import com.basic.basicproject.member.Member;
 import com.basic.basicproject.member.MemberRepository;
 import com.basic.basicproject.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import com.basic.basicproject.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
