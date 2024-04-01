ed485aa8f16d99677d60e44204fd521c6478e62c
Kohyeonjeong <toyu7870@naver.com>
2024-04-01 20:28:08 +0900

    test: All Bean Needed - List & Map

--- src/test/java/com/basic/basicproject/autowired/AllBeanTest.java
+++ src/test/java/com/basic/basicproject/autowired/AllBeanTest.java
@@ -1,13 +1,31 @@
 package com.basic.basicproject.autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.basic.basicproject.AutoAppConfig;
 import com.basic.basicproject.discount.DiscountPolicy;
+import com.basic.basicproject.member.Grade;
 import com.basic.basicproject.member.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import java.util.List;
 import java.util.Map;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AllBeanTest {
+
+    @Test
+    void findAllBean(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L,"userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+    }
+
     static class DiscountService {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
